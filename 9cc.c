6678e16d746ca68063c4314910c5a271b4675ce5
Hideto Maruyama <maruhide@gmail.com>
2021-08-22 10:20:48 +0900

    エラーメッセージを改良

--- 9cc.c
+++ 9cc.c
@@ -26,6 +26,24 @@ struct Token{
 // 現在着目しているトークン
 Token *token;
 
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+// 内容を表示するための改良版
+void error_at(char *loc, char *fmt, ...){
+	va_list ap;
+	va_start(ap, fmt);
+
+	int pos = loc - user_input;
+	fprintf(stderr, "%s\n", user_input);
+	fprintf(stderr, "%*s", pos , " "); // pos個の空白を出力
+	fprintf(stderr, "^ ");
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit(1);
+}
+
 //エラーを報告するための関数
 //printfと同じ引数を取る
 void error(char *fmt, ...){
@@ -49,7 +67,7 @@ bool consume(char op){
 //それ以外の場合にはエラーを報告する
 void expect(char op){
 	if(token->kind != TK_RESERVED || token->str[0] != op)
-		error("'%c'ではありません", op);
+		error_at(token->str, "'%c'ではありません", op);
 	token = token->next; // トークンを１つ読み進める
 }
 
@@ -57,7 +75,7 @@ void expect(char op){
 //それ以外の場合にはエラーを報告する
 int expect_number(){
 	if(token->kind != TK_NUM)
-		error("数ではありません");
+		error_at(token->str, "数ではありません");
 	int val = token->val;
 	token = token->next; // トークンを１つ読み進める
 	return val;
@@ -80,8 +98,10 @@ Token *new_token(TokenKind kind, Token *cur, char *str){
 
 // 入力文字列をトークナイズしてそれを返す
 // この宣言でTokenのポインタを返す
-Token *tokenize(char *p){
-	Token head; // 戻り値
+Token *tokenize(){
+	
+	char *p = user_input; // user_inputをTokenizeするpへ渡す
+ 	Token head; // 戻り値
 	head.next = NULL; // 
 	Token *cur = &head; // 現在のToken位置
 
@@ -112,7 +132,7 @@ Token *tokenize(char *p){
 			continue;
 		}
 
-		error("トークナイズできません。");
+		error_at(p, "トークナイズできません。");
 
 	}
 
@@ -131,7 +151,9 @@ int main(int argc, char **argv){
 	}
 
 
-	token = tokenize(argv[1]);
+	user_input = argv[1]; // エラー表示用
+	token = tokenize(); // tkenize()内でuser_inputを操作してTokenizeしている
+
 
 	// アセンブリの前半部分を出力
 	printf(".intel_syntax noprefix\n");
