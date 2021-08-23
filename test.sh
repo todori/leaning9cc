75f350131f3b146f9dc3965cc0f23cac1caa1b12
Hideto Maruyama <maruhide@gmail.com>
2021-08-23 22:36:20 +0900

    比較演算子の追加

--- test.sh
+++ test.sh
@@ -17,11 +17,25 @@ assert(){
 	fi
 }
 
-# レジスタは負の値を扱えないので、結果が負にならないようにテストを行う
-assert 1 ' -9+10'
-assert 75  ' -5*(-9-6)'
-assert 2 '-4/-2'
-assert 5 '-5/-1'
+assert 1 '0==0'
+assert 0 '1==0'
+assert 1 '1!=2'
+assert 0 '3!=3'
+
+assert 1 '3<4'
+assert 1 '-1 < 0'
+assert 0 ' -2 < -3'
+
+assert 1 '3<=3'
+assert 0 '4<=3'
+
+assert 1 '3>-1'
+assert 0 '9>11'
+
+assert 1 '8>=6'
+assert 1 '8>=8'
+assert 0 '6>=8'
+
 
 echo OK
 
